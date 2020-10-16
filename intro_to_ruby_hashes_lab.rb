6a1135761d2ff8bb37db925c2127a85089050044
Brendan O'Connell <bmoconnell15@gmail.com>
2020-10-16 17:01:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -46,5 +46,32 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-
+- he hash assigned to `:reading_railroad` includes a `:mortgage_value` key set
+  to `100`
+- the hash assigned to `:pennsylvania_railroad` includes a `:mortgage_value` key set to `200`
+- the hash assigned to `:b_and_o_railroad` includes a `:mortgage_value` key set to `400`
+- the hash assigned to `:shortline_railroad` includes a `:mortgage_value` key set to `800`
+    main_hash = {
+    :railroads => {
+      :pieces => 4,
+      :rent_in_dollars => {
+        :one_piece_owned => 25,
+        :two_pieces_owned => 50,
+        :three_pieces_owned => 100,
+        :four_pieces_owned => 200
+      },
+      :names => {
+        :reading_railroad => {
+          :mortgage_value => 100
+        },
+        :pennsylvania_railroad => {
+          :mortgage_value => 200
+        },
+        :b_and_o_railroad => {
+          :mortgage_value => 400
+        },
+        }
+      }
+    }
+  }
 end
