be42eb6eb76069d74273a0c7d680c7281841ee50
Brendan O'Connell <bmoconnell15@gmail.com>
2020-10-16 16:54:02 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,11 +13,12 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
 
-  base_hash
-  main_hash[:railroads] = {
-    :pieces => '4',
-    :rent_in_dollars => {},
-    :names => {}
+  main_hash = {
+    :railroads = {
+      :pieces => '4',
+      :rent_in_dollars => {},
+      :names => {}
+    }
   }
 end
 
